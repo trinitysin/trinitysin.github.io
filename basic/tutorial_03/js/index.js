7ee942cd203cc26928f4c7a506e825cb3ef2b58e
trinitysin <trinitysin@ntnu.edu.tw>
2025-01-13 17:32:37 +0800

    add

--- basic/tutorial_03/js/index.js
+++ basic/tutorial_03/js/index.js
@@ -1,12 +1,45 @@
-console.log("I'm not READY!")
+let map = L.map('map').setView([25.02519625965276, 121.52752194555394], 13);
 
+var infoDiv = document.getElementById('info');
 
-document.addEventListener("DOMContentLoaded", () => {
-    
-    console.log("Loaded Finish!");
-    const button = document.getElementById("openMapButton");
-    button.addEventListener("click", () => {
-        console.log("Hit Hit Hit");
-    });
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+maxZoom: 19,
+attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
+var marker1 = L.marker([25.02519625965276, 121.52752194555394]).addTo(map)
+.bindPopup('這裡是師大文學院!')
+//.openPopup();
+
+var marker2 = L.marker([25.017399653273966, 121.53976297639564]).addTo(map)
+.bindPopup('台大啦weeeeeee!')
+//.openPopup();
+
+var marker3 = L.marker([25.044919992874622, 121.53195354429714]).addTo(map)
+.bindPopup('請正名為吉伊快閃店聚集地!')
+//.openPopup();
+
+marker1.on('click', function () {
+    console.log("Click 1");
+    infoDiv.textContent = "我上班的地方";
+    //infoDiv.textContent = '你點擊了: 師大文學院 ';
+    map.panTo([25.02519625965276, 121.52752194555394]); // 平移到台北 101
 });
+
+marker2.on('click', function () {
+    console.log("Click 2");
+    infoDiv.textContent = "什麼時候才會考得上呢";
+    //infoDiv.textContent = '你點擊了: 台大 ';
+    map.panTo([25.017399653273966, 121.53976297639564]); // 平移到台北 101
+});
+
+marker3.on('click', function () {
+    console.log("Click 3");
+    infoDiv.textContent = "排吉伊排到爛掉";
+    //infoDiv.textContent = '你點擊了: 華山 ';
+    map.panTo([25.044919992874622, 121.53195354429714]); // 平移到台北 101
+});
+
+function goPage(){
+    window.location.href = "https://mira-ar.n010.app/show/XGJFpE";
+}
